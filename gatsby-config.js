ad195f49d82e6f7805943efa43a710c02ceea6e9
Behemoth11 <84832757+Behemoth11@users.noreply.github.com>
2021-12-12 01:04:37 -0600

    hj

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     // {
