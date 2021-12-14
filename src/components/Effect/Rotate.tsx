95750ae5614c458993c4195b70f598bd7123ecd8
Behemoth11 <84832757+Behemoth11@users.noreply.github.com>
2021-12-14 12:23:18 -0600

    continuous integration strategy

--- src/components/Effect/Rotate.tsx
+++ src/components/Effect/Rotate.tsx
@@ -4,47 +4,38 @@ interface Props {
   width: number
   className: string
   children: React.ReactNode
-  speed?: { theta: number; phi: number }
+  rotation?: { theta: number; phi: number }
   init?: { theta: number; phi: number }
 }
 
-const SPEED = (Math.random() + 5) / 5000;
-export const Rotate: React.FC<Props> = ({ children, width, className, speed, init }) => {
-  const [phi, setPhi] = useState((Math.random() + 5) * 100)
-  const [theta, setTheta] = useState((Math.random() + 5) * 100)
 
-  const [vPhi, setVPhi] = useState(SPEED * 1.2)
-  const [vTheta, setVTheta] = useState(SPEED )
-  useEffect(() => {
-    const intervals = [
-      setInterval(() => {
-        setPhi(p => p + vPhi)
-        setTheta(p => p + vTheta)
-      }, 10),
+export const Rotate: React.FC<Props> = ({ children, width, className, rotation, init }) => {
 
-      setInterval(() => {
-        setVPhi(SPEED)
-        setVTheta(SPEED)
-      }, 1000 * 60 *2 ),
-    ]
-
-    return () => intervals.forEach(interval => clearInterval(interval))
-  }, [])
-  
-  console.log(width)
   width = width || 2;
 
-  let x = (width/2 )* Math.cos(theta) * Math.sin(phi)
-  let y = (width/2) * Math.sin(theta) * Math.sin(phi)
-  let z = (width/2) * Math.cos(phi)
+  let x = (width/2 )* Math.cos(init.theta + rotation.theta) * Math.sin(init.phi + rotation.phi)
+  let y = (width/2) * Math.sin(init.theta+ rotation.theta) * Math.sin(init.phi + rotation.phi)
+  let z = 100 * Math.cos(init.phi + rotation.phi)
 
 
-  // console.log(x, y, z)
+  // console.log(x, y , z)
+
+    // let x = (width/2 )* Math.cos(theta) * Math.sin(phi)
+  // let y = (width/2) * Math.sin(theta) * Math.sin(phi)
+  // let z = (width/2) * Math.cos(phi)
+
+  // console.log(init, children)
+  // console.log(x, y )
+
+  // console.log(rotation.phi, rotation.theta)
+
+  // console.log(`hsla(10, 10%, 10%, ${z/ (width/2) + 1}})`)
   return (
     <div
       className={`absolute rounded-full p-2  left-1/2 top-1/2  ${className}`}
       style={{
         transform: `translate3d(calc( ${x}px - 50%), calc( ${y}px - 50%) , ${z}px)`,
+        color: `hsla(100, 100%, 100%, ${z/ (100*2)+0.5})`
       }}
     >
       {children}
--- src/components/Effect/preset/TurningWord.tsx
+++ src/components/Effect/preset/TurningWord.tsx
@@ -1,50 +1,115 @@
 import React, { useEffect, useMemo, useRef, useState } from "react"
 import { Rotate } from "../Rotate"
 
-
-const DENSITY = 2
+const DENSITY = 20
 
 const create_skill = (arr, density) => {
   const result = []
 
   for (let i = 0; i < density; i++) {
-   result.push(...arr)
+    result.push(...arr)
   }
 
   console.log("I am running")
-  return result;
-  
+  return result
 }
 
-const SKILLS = create_skill(["python", "javascript", "react", "mongo", "mysql", "java"], DENSITY)
-// console.log(SKILL)
+const SKILLS = create_skill(
+  ["python", "javascript", "react", "mongo", "mysql", "java"],
+  DENSITY
+)
+
+const getInits = (quantity, start) => {
+  const PI = Math.PI
+
+  const phi_spacing = (2 * PI) / quantity
+  const matrixes = []
+
+  for (const phi of [0.1 * PI, 0.3* PI, 0.5 * PI , 0.8 * PI]) {
+    for (let theta = start; theta < 2 * PI + start; theta += phi_spacing) {
+      matrixes.push({ phi, theta })
+    }
+  }
+  return matrixes
+}
+
 interface Props {
-  classNameWord: string,
+  classNameWord: string
   className: string
 }
 
+const Matrix = getInits(6, 0)
+const SPEED = (Math.random() + 5) / 9000
+
 //setting position absolute or relative in className is required for this to work;
-const TurningWord = ({classNameWord, className}) => {
+const TurningWord = ({ classNameWord, className }) => {
+  const ctnRef = useRef(null)
+  const [width, setWidth] = useState(400)
 
-    const ctnRef = useRef(null)
-    const [width, setWidth] = useState(400)
+  useEffect(() => {
+    setWidth(ctnRef.current.clientWidth)
+  }, [])
+  console.log(Matrix.map(m => ({phi: m.phi * 180/Math.PI, theta : m.theta * 180/Math.PI})))
 
-    useEffect(() => {
-        setWidth(ctnRef.current.clientWidth)
-    }, [])
+  const [phi, setPhi] = useState(0)
+  const [theta, setTheta] = useState(0)
+
+  const [vPhi, setVPhi] = useState(SPEED)
+  const [vTheta, setVTheta] = useState(SPEED)
+  useEffect(() => {
+    const intervals = [
+      setInterval(() => {
+        setPhi(p => {
+          
+          let nextPhi = p + vPhi
+          if (nextPhi >= 2 * Math.PI){
+            nextPhi = 0;
+          }
+          return nextPhi;
+        })
+        setTheta(p => p + vTheta)
+      }, 10),
+
+      setInterval(() => {
+        setVPhi(SPEED)
+        setVTheta(SPEED)
+      }, 1000 * 60 * 2),
+    ]
+
+    return () => intervals.forEach(interval => clearInterval(interval))
+  }, [])
+
   return (
     <div
       style={{ perspective: "400px" }}
       className={`overflow-hidden view ${className}`}
       ref={ctnRef}
     >
-      {SKILLS.map( skill => (
-        <Rotate width={width} className={classNameWord}>
-          {skill}
+      {Matrix.map( (init, index) => (
+        <Rotate width={400} className={classNameWord} rotation={{phi, theta}} init={{phi: init.phi, theta: init.theta}} >
+          {SKILLS[index]}
         </Rotate>
       ))}
+
+      {/* {/* <Rotate
+        width={200}
+        className={classNameWord}
+        rotation={{ phi, theta }}
+        init={{ phi: 0, theta: 0}}
+      >
+        {"SKILLS[index]"}
+      </Rotate> */}
+{/* 
+      <Rotate
+        width={200}
+        className={" bg-black "}
+        rotation={{ phi, theta }}
+        init={{ phi: 0.5 * Math.PI, theta:0 }}
+      >
+        {"SKILLS[index] "}
+      </Rotate> */}
     </div>
   )
 }
 
-export default TurningWord;
+export default TurningWord
