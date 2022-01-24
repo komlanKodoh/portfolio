5aed3b339b27c0d85aa7f3361a76bdf3d9efbe3b
Behemoth11 <kodohdaniel@gmail.com>
2022-01-24 09:04:35 -0600

    continuous integration strategy

--- src/components/BuildingBlocks/ShowProject.tsx
+++ src/components/BuildingBlocks/ShowProject.tsx
@@ -3,30 +3,31 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { LinkButton } from "../Basic/Button";
 
 const ShowProject = ({ project, right }) => {
-
   return (
     <div className="grid sm:grid-cols-2 grid-cols-1 gap-x-12 place-items-center my-12">
-      <div className="relative rounded-md overflow-hidden w-full shadow-2xl  shadow-black">
+      <div className="relative rounded-md overflow-hidden  w-full shadow-xl shadow-black">
+
         <GatsbyImage
-          className=""
+          className="bg-black"
           image={project.previewImage?.gatsbyImageData}
           alt={project.previewImage?.title}
         />
-        <div className="absolute w-full bg-red top-0 left-0 bg-slate-900"></div>
       </div>
-      <div className={`flex ${right && "col-start-1  row-start-1"} -sm:col-start-1 -sm:row-start-2`}>
+      <div
+        className={`flex ${
+          right && "col-start-1  row-start-1"
+        } -sm:col-start-1 -sm:row-start-2`}
+      >
         <div className="my-auto sm:text-right ">
           <h2 className=" text-2xl my-4">{project.name}</h2>
           <ul className="sm:flex flex-start justify-end gap-2  my-2 flex-wrap">
-            {project.techStack?.sort().map(
-              (item) => (
-                <li key={item}>
-                  <div className=" rounded-md px-4 py-0 bg-black inline-block my-1">
-                    {item}
-                  </div>
-                </li>
-              )
-            )}
+            {project.techStack?.sort().map((item) => (
+              <li key={item}>
+                <div className=" rounded-md px-4 py-0 bg-black inline-block my-1">
+                  {item}
+                </div>
+              </li>
+            ))}
           </ul>
           <p className="text-justify text-sm leading-7">
             {project.description}
