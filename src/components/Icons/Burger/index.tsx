6e4e56ef62efcbfb45813d35abc4effc944b1541
Behemoth11 <kodohdaniel@gmail.com>
2022-01-24 14:07:37 -0600

    fixe to the bug with the burger icon and moving overlay to remove the need to add a burger button in the overlay. Also makes the transition more fluid

--- src/components/Icons/Burger/index.tsx
+++ src/components/Icons/Burger/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import * as styles from "./style.module.scss";
+import { useFirstTimeLoading } from "../../../lib/hooks";
 
 interface Props
   extends React.DetailedHTMLProps<
@@ -28,22 +29,20 @@ const Burger: React.FC<Props> = ({
 }) => {
   const [myState, setMyState] = useState("close");
 
-  //   const onClick = () => {
-  //     setMyState("mid")
-  //   }
+  const firstTimeLoading = useFirstTimeLoading()
 
   useEffect(() => {
+    if (firstTimeLoading) return;
     setMyState((prev) => (prev === "mid" ? map[`${state}`] : "mid"));
   }, [state]);
 
-  // const b_style = "w-8 h-1 bg-white"
+
   return (
     <div
       {...props}
       onClick={onClick}
       className={` w-fit p-1 flex-col flex justify-around relative h-full z-20 ${className}`}
     >
-      {/* <div className={b_style}></div> */}
       <div
         className={`${styles.bar} ${styles[myState]} relative `}
         onTransitionEnd={() => {
@@ -52,19 +51,19 @@ const Burger: React.FC<Props> = ({
       >
         <div className={classNameBar}></div>
         <div className={classNameBar}></div>
-        <div className={classNameBar}></div>
+        <div
+          className={classNameBar}
+          style={{
+            opacity:
+              myState === "close" || (myState === "mid" && state === true                  )
+                ? 1
+                : 0,
+          }}
+        ></div>
       </div>
-      {/* <div className={b_style}></div> */}
     </div>
   );
 };
 
-// const Bar: React.FC<{ className: string }> = ({ className }) => {
-//   return (
-//     <div
-//       className={`absolute top-1/2 left-1/2  h-1 w-90p bg-blue-50 -translate-x-1/2 ${className}`}
-//     ></div>
-//   )
-// }
 
 export default Burger;
