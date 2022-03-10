8ce931da0313cb2f0d109352b4b8da74a1b054a9
Behemoth11 <kodohdaniel@gmail.com>
2022-03-09 21:16:54 -0600

    continuous integration strategy

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,8 @@ import * as React from "react"
 
 const NotFoundPage = () => (
   <div className="h-screen flex">
-    <div className="h-fit m-auto p-4">
-      <h1>404: Not Found</h1>
-      <p>
-        Sorry to see you here honey, press the link to go back to the home Page
-      </p>
+    <div className="h-fit m-auto p-4 text-white text-7xl">
+      <h1>Sorry this page does not exist</h1>
       <a href="/">To Home Page</a>
     </div>
   </div>
